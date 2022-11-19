90bf2be90641d83e1e20843766321d7467e574e2
daspartho <parthodas6176@gmail.com>
2022-11-19 11:41:54 +0530

    Added examples of prompts and completions

--- main.py
+++ main.py
@@ -18,13 +18,33 @@ def suggest(
 
     prompt=f"""This app suggests gifts based on several factors.
 
+    Age: 6
+    Gender: female
+    Interests: Disney
+    Gift Suggestions: Disney princess dress, Disney Frozen toy, A trip to Disney World
+    --
+    Age: 25
+    Gender: male
+    Interests: Cooking
+    Gift Suggestions: A nice set of kitchen knives, A subscription to a cooking magazine, A cooking apron
+    --
+    Age: 81
+    Gender: female
+    Interests: Gardening
+    Gift Suggestions: A gardening apron or gloves, A book on gardening, a gift certificate to a local nursery or garden shop
+    --
+    Age: 17
+    Gender: male
+    Interests: Anime
+    Gift Suggestions: A gift card to an anime or manga store, A DVD or Blu-ray set of an Anime show, an Anime t-shirt or hoodie, a Funko Pop figure of an Anime character
+    --
     Age: {age}
     Gender: {gender}
     Interests: {interest}
-    Gift Suggestion:"""
+    Gift Suggestions:"""
 
     response = co.generate(prompt=prompt)
-    return response.generations[0].text.split('\n')[0]
+    return response.generations[0].text
 
 if __name__ == "__main__":
-    print(suggest(25, 'Male', 'Cooking'))
+    print(suggest(10, 'male', 'Basketball'))
